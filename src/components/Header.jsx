40dd2e3a7f24804dd9bc6d18e6a2c05f5d3ef2ec
karanhub003 <karangithub003@gmail.com>
2026-04-28 12:23:45 +0530

    Header and home components is made up and header basic styling is done

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import logoImg from "../assets/logo.png";
+import menuIcon from "../assets/menu.png";
+import './Header.css'
+
+
+
+export default function Header() {
+  return (
+    <div className="navBar">
+      <div className="logoBox">
+        <img className="logoImg" src={logoImg} alt="Logo" />
+        <p>NEXORA</p>
+      </div>
+      <div className="searchContainer">
+          <i className="fa-solid fa-magnifying-glass"></i>
+          <input type="text" placeholder="Search Nexora.in"/>
+      </div>
+      <div className="linkContainer">
+        <div id="otherLink"  className="navLinks">
+        <p>Category</p>
+      </div>
+      <div id="otherLink" className="navLinks">
+        <p>Contact Us</p>
+      </div>
+      <div className="navLinks">
+        <i class="fa-solid fa-cart-shopping"></i>
+        <p>My Cart</p>
+      </div>
+      <div className="menuIcon">
+        <img src={menuIcon} alt="menuIcon" />
+      </div>
+      </div>
+    </div>
+  );
+}
